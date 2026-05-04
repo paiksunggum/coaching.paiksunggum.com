98320713ec3c06fdec77672957e14f05991d7664
paiksunggum <bsgum5373@gmail.com>
2026-05-04 09:38:58 +0900

    첫번째 커밋

--- apps/titanic_starter.py
+++ apps/titanic_starter.py
@@ -3,5 +3,9 @@ import pandas as pd
 
 csv_path = Path(__file__).parent / "Titanic-Dataset.csv"
 df = pd.read_csv(csv_path)
+
 print(df.head())
+print("-" * 30)
 print(df.shape)
+print("-" * 30)
+df.info()
