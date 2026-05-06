b383515e825aa26e0b07ad5cf529cf7614f8ebe4
paiksunggum <bsgum5373@gmail.com>
2026-05-06 16:49:53 +0900

    포르마 추가

---
+++ apps/doro/app/doro_director.py
@@ -0,0 +1,12 @@
+from doro_reader import Doro_reader
+
+
+class Doro_director:
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    print("도로 디렉터가 메인이다.")
+    d = Doro_reader()
+    d.get_data()
